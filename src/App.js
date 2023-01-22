644e4c649206e029e9f60d2b772b97907720fd0e
Anuj Kumar <anuj9kumar.ak@gmail.com>
2023-01-22 13:41:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Gallery from "./Gallery";
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Image Gallery</h1>
+      <h2 style={{ textAlign: "center" }}>Please Subscribe Code </h2>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
